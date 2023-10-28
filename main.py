a048d873dbdfd819294f049da178ff6e724f99c6
bhupendrak9917 <bhupendrakumar1545@gmail.com>
2023-10-29 00:00:44 +0530

    evaluation added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ChickenDiseaseClassification import logger
 from ChickenDiseaseClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ChickenDiseaseClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from ChickenDiseaseClassification.pipeline.stage_03_training import ModelTrainingPipeline
+from ChickenDiseaseClassification.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -36,3 +37,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
